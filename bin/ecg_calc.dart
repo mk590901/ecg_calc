9800673f02aea93ef8503ac1cdd8c814568c5ebc
michael-k <michaelsure9@gmail.com>
2025-04-24 15:02:40 +0300

    update

--- bin/ecg_calc.dart
+++ bin/ecg_calc.dart
@@ -1,5 +1,5 @@
 import 'package:ecg_calc/ecg_calc.dart' as ecg_calc;
 
 void main(List<String> arguments) async {
-  print('Hello world: ${ecg_calc.calculate()}!');
+  print('ECG calculation result -> [${ecg_calc.calculate()}]');
 }
--- lib/ecg_calc.dart
+++ lib/ecg_calc.dart
@@ -2,10 +2,9 @@ import 'ecg_calc_imp.dart';
 import 'ecg_param.dart';
 import 'ecg_log.dart';
 
-int calculate() {
+bool calculate() {
   EcgCalc calc = EcgCalc(EcgParam(), EcgLogWindow());
   bool rc = calc.calculateEcg();
-  print ('rc=$rc');
-  return 0;
+  return rc;
 }
 
